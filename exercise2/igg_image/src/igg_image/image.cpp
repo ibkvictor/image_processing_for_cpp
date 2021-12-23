9092d4f745ec829bfdc62c1c18ba559b784a6eac
ibkvictor <eze99victor@gmail.com>
2021-12-23 13:29:36 +0100

    file stream input system initialized

--- exercise2/igg_image/src/igg_image/image.cpp
+++ exercise2/igg_image/src/igg_image/image.cpp
@@ -1,33 +1,29 @@
-
+#include "image.h"
 #include <vector>
+#include <iostream>
+using namespace igg;
 
-namespace igg {
-
-class Image {
- private:
-  int rows_ = 0;
-  int cols_ = 0;
-  const int max_val_ = 255;
-  std::vector<vector<int> data_;
- public:
-  Image(){
+Image::Image(){
    cols_ = 0;
    rows_ = 0;
-  }
-  Image(int rows, int cols){
+}
+
+Image::Image(int rows, int cols){
    rows_ = rows;
    cols_ = cols;
-   std::vector<vector<int>> data_ (rows, std::vector<int> (cols, 0));
-  };
-  int &at (int rows, int cols){
+   std::vector <std::vector<int>> v (rows, std::vector<int> (cols, 0));
+   data_ = v;
+}
+int &Image::at (int rows, int cols){
    return data_[rows][cols];
-  }
-  std::vector ComputeHistogram(int bins){
+}
+
+std::vector<int> Image::ComputeHistogram(int bins){
    std::vector<int> result(bins, 0);
    for(auto &i: data_){
     for(auto &j: i){
      for(auto k=1 ; k<=bins; k++){
-      if(j < (max_val * k)/bins){
+      if(j < (max_val_ * k)/bins){
        result[k-1] += 1;
        break;
       }
@@ -35,6 +31,9 @@ class Image {
     }
    }
    return result;
-  }
 }
+
+std::vector<int> Image::get_size(){
+  std::cout<<rows_<<"; "<<cols_<<std::endl;
+  return std::vector<int> {rows_, cols_};
 }
--- exercise2/igg_image/src/igg_image/image.h
+++ exercise2/igg_image/src/igg_image/image.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 #include <vector>
 
 namespace igg {
@@ -9,12 +10,15 @@ class Image {
   Image(); //image constructor when size of image is not specified
   Image(int rows, int cols); //image constructor using defined n by n size of image
   int &at (int rows, int cols); //gets the pixel value at the specified index and assigns values to that index
-  std::vector ComputeHistogram(int bins); //computes the histogram of the image
+  std::vector<int> ComputeHistogram(int bins); //computes the histogram of the image
+  std::vector<int> get_size();
+  bool FillFromPgm(const std::string& file_name);
+  void WriteToPgm(const std::string& file_name);
  private:
-  int rows_ = 0;
-  int cols_ = 0;
+  int rows_;
+  int cols_;
   int max_val_ = 255; //maximum possible pixel value
-  std::vector<vector<int> data_; //2d vector containing the image pixel values
+  std::vector<std::vector<int>> data_; //2d vector containing the image pixel values
 };
 
 }  // namespace igg
