5bc685377793e1a80a8bbdc09c5279373608e21f
ibkvictor <eze99victor@gmail.com>
2021-12-23 09:00:02 +0100

    exercise 2 first attempt

--- exercise2/igg_image/src/igg_image/image.h
+++ exercise2/igg_image/src/igg_image/image.h
@@ -7,11 +7,22 @@ namespace igg {
 class Image {
  public:
   ///////////////////// Create the public interface here ///////////////////////
+  Image(){
+   cols_ = 0;
+   rows_ = 0;
+  };
+  Image(int rows, int cols){
+   rows_ = rows;
+   cols_ = cols
+  };
+  int &at (int rows, int cols){
+   return data_[rows][cols]; 
+  }
  private:
   int rows_ = 0;
   int cols_ = 0;
   int max_val_ = 255;
-  std::vector<int> data_;
+  std::vector<vector<int> data_;
 };
 
 }  // namespace igg
