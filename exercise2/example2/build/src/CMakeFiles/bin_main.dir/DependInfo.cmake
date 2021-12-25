95a90c1a75932e0ba02b1d907c21bac3a54d2662
ibkvictor <eze99victor@gmail.com>
2021-12-25 14:01:10 +0100

    editing cmake configuration file for include directories

---
+++ exercise2/example2/build/src/CMakeFiles/bin_main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/user/Documents/image_processing_cpp_exercies/exercise2/example2/src/main.cpp" "/Users/user/Documents/image_processing_cpp_exercies/exercise2/example2/build/src/CMakeFiles/bin_main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/user/Documents/image_processing_cpp_exercies/exercise2/example2/build/src/CMakeFiles/lib_sqroot.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ exercise2/example2/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/user/Documents/image_processing_cpp_exercies/exercise2/example2/src
+# Build directory: /Users/user/Documents/image_processing_cpp_exercies/exercise2/example2/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
