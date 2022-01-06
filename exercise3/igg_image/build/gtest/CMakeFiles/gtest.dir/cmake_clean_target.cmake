97a9acce0e913d61467d3883981872cab203d044
ibkvictor <eze99victor@gmail.com>
2022-01-06 12:28:13 +0100

    image class compiled successfully

---
+++ exercise3/igg_image/build/gtest/CMakeFiles/gtest.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/Users/user/Documents/image_processing_cpp_exercies/exercise3/igg_image/lib/libgtest.a"
+)
---
+++ exercise3/igg_image/src/igg_image/io_strategies/ppm_strategy.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "igg_image/io_strategies/strategy.h"
+#include <fstream>
+
+namespace igg {
+
+class PpmIoStrategy: public IoStrategy{
+	bool Write(const std::string& file_name, const ImageData& data) const override;
+
+	ImageData Read(const std::string& file_name) const override;
+};
+
+}//namespace igg
