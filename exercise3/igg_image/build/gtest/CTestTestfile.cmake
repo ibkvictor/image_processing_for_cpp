a6efc0b0924e6e844efd96bc50d3e4a3ff855d6d
ibkvictor <eze99victor@gmail.com>
2022-01-17 20:34:07 +0100

    testing: constructor error fixed

---
+++ exercise3/igg_image/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/user/Documents/image_processing_cpp_exercies/googletest-release-1.8.0/googletest
+# Build directory: /Users/user/Documents/image_processing_cpp_exercies/exercise3/igg_image/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ exercise3/igg_image/build/tests/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/user/Documents/image_processing_cpp_exercies/exercise3/igg_image/tests
+# Build directory: /Users/user/Documents/image_processing_cpp_exercies/exercise3/igg_image/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(color_image_test "/Users/user/Documents/image_processing_cpp_exercies/exercise3/igg_image/bin/color_image_test")
+set_tests_properties(color_image_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/user/Documents/image_processing_cpp_exercies/exercise3/igg_image/tests/CMakeLists.txt;19;add_test;/Users/user/Documents/image_processing_cpp_exercies/exercise3/igg_image/tests/CMakeLists.txt;0;")
+subdirs("../gtest")
