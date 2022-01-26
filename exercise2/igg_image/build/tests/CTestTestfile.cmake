0fd0dd7300a402c30abcd4ba33e4013473dea568
ibkvictor <eze99victor@gmail.com>
2022-01-26 19:57:01 +0100

    store files

--- exercise2/igg_image/build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/user/Documents/image_processing_cpp_exercies/exercise2/igg_image/tests
-# Build directory: /Users/user/Documents/image_processing_cpp_exercies/exercise2/igg_image/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(initial_matrix_test "/Users/user/Documents/image_processing_cpp_exercies/exercise2/igg_image/bin/initial_matrix_test")
-set_tests_properties(initial_matrix_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/user/Documents/image_processing_cpp_exercies/exercise2/igg_image/tests/CMakeLists.txt;21;add_test;/Users/user/Documents/image_processing_cpp_exercies/exercise2/igg_image/tests/CMakeLists.txt;0;")
-subdirs("../gtest")
